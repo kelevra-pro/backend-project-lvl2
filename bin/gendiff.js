4ce290440f1bbbfae117ce337f3dec4dc0d278b6
kelevra-pro <damtat8@gmail.com>
2022-08-11 00:27:10 +0300

    fix lint

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,6 +3,6 @@ const { program } = require('commander');
 
 program
   .version('1.0.0')
-  .description('Compares two configuration files and shows a difference.')
+  .description('Compares two configuration files and shows a difference.');
 
 program.parse();
